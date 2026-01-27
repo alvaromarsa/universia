845d676f154a0c1f4166144039113432ab79e920
Alvaro Martinez <alvaromarsa1985@gmail.com>
2026-01-27 08:00:34 +0100

    Workspace agregado a github

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+})
+export class Navbar { }
